074d8cf455640fe9fec68a40a1f76743e0a3a373
Sasmit <sasmit.joshi21@gmail.com>
2023-06-18 21:35:50 +1000

    Started assetReturn function

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 # Function that returns an assets price after the return is
 # applied to it
@@ -5,3 +6,9 @@ def assetValue(assetPrice, returnValue):
     return assetPrice * (1 + returnValue)
 
 
+# Function that generates a random number between -1 and 1
+# to mimick the return on assets based on a probability distribution
+def assetReturn(standardDeviation, mean):
+    
+
+    return 0
