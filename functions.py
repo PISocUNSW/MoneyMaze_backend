37e0fd4e0e042416025b23e2064a037c861ff744
Sasmit <sasmit.joshi21@gmail.com>
2023-06-19 16:55:38 +1000

    Removed unncecessary comment

--- functions.py
+++ functions.py
@@ -15,9 +15,6 @@ def assetValue(assetPrice, returnValue):
     return assetPrice * (1 + returnValue)
 
 
-# Function that generates a random number between -1 and 1
-# to mimick the return on assets based on a probability distribution
-
 """ 
     Generates a random number between -1 and 1 to mimick the return on assets based 
     on a probability distribution.
