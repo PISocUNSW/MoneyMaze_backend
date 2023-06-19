7fc51b1428d9bd3124f30292e8c647a7105eef02
Sasmit <sasmit.joshi21@gmail.com>
2023-06-19 16:55:06 +1000

    Added documentation for functions

--- functions.py
+++ functions.py
@@ -1,13 +1,34 @@
 import numpy as np
 
-# Function that returns an assets price after the return is
-# applied to it
+""" 
+    Returns an asset's price after the asset return is applied to it.
+
+    Args:
+        assetPrice (double): Price of the asset.
+        returnValue (double): Return of the asset.
+
+    Returns:
+        double: The asset's price after the asset return is applied to it.
+"""
+
 def assetValue(assetPrice, returnValue):
     return assetPrice * (1 + returnValue)
 
 
 # Function that generates a random number between -1 and 1
 # to mimick the return on assets based on a probability distribution
+
+""" 
+    Generates a random number between -1 and 1 to mimick the return on assets based 
+    on a probability distribution.
+
+    Args:
+        standardDeviation (double): The standard deviation of our probability distribution.
+        mean (double): The mean of your probability distribtuion.
+
+    Returns:
+        double: A number between -1 and 1 based on a probability distribution
+"""
 def assetReturn(standardDeviation, mean):
     
 
