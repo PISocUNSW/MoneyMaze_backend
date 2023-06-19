09ac1c0d81ee509d00f8aa9c8811a4302346edcb
Sasmit <sasmit.joshi21@gmail.com>
2023-06-19 19:25:04 +1000

    Attempted assetReturn function

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+# Imported Libraries
 import numpy as np
 
 """ 
@@ -10,23 +11,24 @@ import numpy as np
     Returns:
         double: The asset's price after the asset return is applied to it.
 """
-
 def assetValue(assetPrice, returnValue):
     return assetPrice * (1 + returnValue)
 
+
+#######################################################################################################################
 
 """ 
     Generates a random number between -1 and 1 to mimick the return on assets based 
     on a probability distribution.
 
     Args:
-        standardDeviation (double): The standard deviation of our probability distribution.
-        mean (double): The mean of your probability distribtuion.
+        mean (double): The mean of the asset's return in the probability distribution.
+        standardDeviation (double): The standard deviation of the asset's return in the probability distribution.
 
     Returns:
         double: A number between -1 and 1 based on a probability distribution
 """
-def assetReturn(standardDeviation, mean):
-    
+def assetReturn(mean, standardDeviation):
+    assetReturns = np.random.normal(mean, standardDeviation)
+    return assetReturns
 
-    return 0
